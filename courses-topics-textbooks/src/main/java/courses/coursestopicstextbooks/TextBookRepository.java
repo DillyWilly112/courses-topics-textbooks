a37d7dd93ec798e5ea477a0ba1ed60b30acb8cbb
Dylan Vaca <58199918+DillyWilly112@users.noreply.github.com>
2020-02-29 20:18:46 -0500

    Add files via upload
    First Commit

---
+++ courses-topics-textbooks/src/main/java/courses/coursestopicstextbooks/TextBookRepository.java
@@ -0,0 +1,7 @@
+package courses.coursestopicstextbooks;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TextBookRepository extends CrudRepository<Textbook, Long> {
+
+}
---
+++ courses-topics-textbooks/src/test/java/courses/coursestopicstextbooks/CoursesTopicsTextbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package courses.coursestopicstextbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesTopicsTextbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
